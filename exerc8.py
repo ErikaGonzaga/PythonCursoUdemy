4e2da806e42a7f6f240606bf77ad77748097fba9
ErikaGonzaga <erikagonzaga@alu.ufc.br>
2021-05-17 16:47:37 -0300

    created a new file exerc8

---
+++ exerc8.py
@@ -0,0 +1,19 @@
+lista = [20,58,1,3,5,100,54,65,96,7,2]
+lista2 = ["dinheiro", "abacate", "bola"]
+
+print(lista)
+
+# lista.sort()
+# print(lista)
+
+# lista_ordenada = sorted(lista)
+# print(lista_ordenada)
+
+# lista.sort(reverse=True)
+# print(lista)
+
+# lista.reverse()
+# print(lista)
+
+lista2.sort()
+print(lista2)
