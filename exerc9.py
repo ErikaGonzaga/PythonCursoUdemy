c03061a17325a8ee33ae54cece8db308ca0ca808
ErikaGonzaga <erikagonzaga@alu.ufc.br>
2021-05-17 17:30:07 -0300

    created a new file exerc9

---
+++ exerc9.py
@@ -0,0 +1,17 @@
+#dicionário
+meu_dicionario = {"A":"Ameixa","B":"Bola","C":"Cachorro"}
+
+# print(meu_dicionario["A"])
+# print(meu_dicionario)
+
+# for chave in meu_dicionario:
+#     print(chave+"-"+meu_dicionario[chave])
+
+for i in meu_dicionario.items():
+    print(i)
+
+for i in meu_dicionario.keys():
+    print(i)
+
+for i in meu_dicionario.values():
+    print(i)
