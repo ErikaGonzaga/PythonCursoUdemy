d753022fa63f108ed2d24e1d98d7958696c760aa
ErikaGonzaga <erikagonzaga@alu.ufc.br>
2021-05-17 16:28:22 -0300

    created a new file exec7

---
+++ exerc7.py
@@ -0,0 +1,29 @@
+lista = ["abacaxi","melancia","abacate"]
+lista2 = [1,2,3,4,5]
+lista3 = ["abacaxi", 2, 9.89,True]
+
+print(lista2[1])
+
+# for item in lista:
+#     print(item)
+
+# tamanho = len(lista3)
+# print(tamanho)
+
+# lista.append("limao")
+# print(lista)
+
+# if 7 in lista2:
+#     print("7 está na lista")
+
+# if 'abacaxi' in lista:
+#     print("abacaxi esta na lista")
+
+#remover item da lista
+# del lista2[3:]
+# print(lista2)
+
+lista4 = []
+lista4.append(34)
+print(lista4)
+
