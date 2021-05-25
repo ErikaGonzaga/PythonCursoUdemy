1016e2abe3968222a8fe904fb85df9925a7f43db
ErikaGonzaga <erikagonzaga@alu.ufc.br>
2021-05-24 22:05:26 -0300

    new file

---
+++ exerc14.py
@@ -0,0 +1,9 @@
+#Escreva um programa que compare se duas sequências digitadas pelo usuário são iguais
+
+lista1 = input("Informe a primeira lista: ")
+lista2 = input("Informe a segunda lista: ")
+
+if(lista1 == lista2):
+    print("As listas são iguais")
+else:
+    print("As listas são diferentes")
